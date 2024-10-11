a79f3e31b94a23e29a78cf0b94efb2dfb7145b11
Tabishdawood45 <Tabishdawood54@gmail.com>
2024-10-11 20:00:00 +0300

    change port 4000 to 3000

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
